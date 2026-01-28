b709f84053b64caef44c7a96f0d96c0a047e83e3
Andres Vargas <avargas@intertalksystems.com>
2026-01-27 23:06:27 -0400

    wip

--- python/0189.rotate-array/solution.py
+++ python/0189.rotate-array/solution.py
@@ -7,18 +7,25 @@ from leetgo_py import *
 
 # @lc code=begin
 
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
-        Do not return anything, modify nums in-place instead.
+        #Input: nums = [1,2,3,4,5,6,7], k = 3
+                        ^     ^     ^ i=0
+                          ^     ^     i=1
+                            ^     ^   i=2
+                              ^     ^ i=3  7//3 = 2
+        ANSWER         [5,6,7,1,2,3,4]
         """
-        
+        nums[:] = nums[len(nums) - k :] + nums[:k]
+
 
 # @lc code=end
 
 if __name__ == "__main__":
     nums: List[int] = deserialize("List[int]", read_line())
     k: int = deserialize("int", read_line())
-    rotate(nums, k)
+    Solution().rotate(nums, k)
     ans = nums
     print("\noutput:", serialize(ans, "List[int]"))
