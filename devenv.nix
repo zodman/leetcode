c9cf3099aeb23d34a1e7c51d3a5015d25661915f
Andres Vargas <avargas@intertalksystems.com>
2026-02-03 16:47:50 -0400

    wip

--- devenv.nix
+++ devenv.nix
@@ -10,12 +10,17 @@
   languages.python.enable = true;
   languages.python.venv.enable = true;
   languages.python.venv.requirements = ./python/requirements.txt;
-  
+
   dotenv.enable = true;
 
   packages = [
     pkgs.leetgo
     pkgs.just
   ];
+
+  enterShell = ''
+    python -m venv ./python/.venv/
+    ./python/.venv/bin/pip install -r ./python/requirements.txt  
+  '';
 
 }
